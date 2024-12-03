4d1339b72a52fddeb135f5e1e1832ec5c063e9e1
Jared Mehle <jrmehle@gmail.com>
2024-12-03 00:29:55 -0600

    day 2 part 2

---
+++ day-1/day1-part2.rb
@@ -0,0 +1,22 @@
+# read input file and organize data into columns
+left_col = []
+right_col = []
+
+File.readlines('input', chomp: true).each do |line|
+  left, right = line.split
+  left_col << left
+  right_col << right
+end
+
+total = 0
+right_tallies = right_col.tally
+# iterate over left list
+left_col.each do |left_val|
+  # count times item appears in right list
+  # multiply number by count and add to total
+  right_count = right_tallies[left_val] || 0
+  total += left_val.to_i * right_count
+end
+
+# output total
+puts total
